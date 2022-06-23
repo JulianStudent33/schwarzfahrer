24f9f75ad551baa62dec2f0913476dee169dd69c
Nbarbaa26 <72656488+Nbarbaa26@users.noreply.github.com>
2022-06-23 10:31:34 +0200

    Passwort klasse test

---
+++ src/nickcode/pass.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class pass {
+    private static boolean containsUpperCase;
+
+    public static void main(String[] args) {
+
+        passwordinput();
+        }
+
+    public static String passwordinput(){
+        String input = JOptionPane.showInputDialog("Passwort eingeben");
+        if (passwordOk(input) == false){
+            System.out.println("Passwort ungueltig! \n Password muss enthalten: \n 6-10 zeichen \n Mindestens 1 Großbuchstabe \n Mindestens 1 Kleinbuchstabe \n Mindestens eine Zahl");
+            passwordinput();
+        }else{
+            System.out.println("Passwort gesetzt");
+        }
+        return input;
+    }
+    public static boolean passwordOk(String password){
+        if (password == null) return false;
+        if (password.length() < 6 || password.length() > 10) return false;
+        boolean containsUpperCase = false;
+        boolean containsLowerCase = false;
+        boolean containsDigit = false;
+        for(char ch: password.toCharArray()){
+            if(Character.isUpperCase(ch)) containsUpperCase = true;
+            if(Character.isLowerCase(ch)) containsLowerCase = true;
+            if(Character.isDigit(ch)) containsDigit = true;
+        }
+        return containsUpperCase && containsLowerCase && containsDigit;
+    }
+}
