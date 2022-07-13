4f5147cb49ea4bc7b38020491538815b9dbb6b51
DenKedi <s_bleck21@hwr-berlin.de>
2022-07-13 17:35:27 +0200

    Erfassen GUI grobe Form für Schwarzfahrt erfassen. Registrierung GUI hat jetzt alle Inputfelder für Sachbearbeiter und Kontrolleur. Wird auch gleich so gespeichert.

--- src/GUI/Kontrolleur/erfassen.java
+++ src/GUI/Kontrolleur/erfassen.java
@@ -1,10 +1,10 @@
 package src.GUI.Kontrolleur;
 
-import src.Foo;
 import src.users.Kontrolleur;
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class erfassen extends JFrame{
 
@@ -32,6 +32,7 @@ public class erfassen extends JFrame{
     private JComboBox comboBox2;
     private JToggleButton speichernToggleButton;
     private JLabel nameLabel;
+    private JToggleButton abbrechenToggleButton;
 
     public erfassen(){
         this.currentUser = KontrolleurGUI.currentUser;
@@ -39,9 +40,10 @@ public class erfassen extends JFrame{
         setTitle("Kontrolleur Menü");
         Point center = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
         int width = 1000;
-        int height = 600;
+        int height = 400;
         this.setBounds(center.x - width / 2, center.y - height / 2, width, height);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
         try {
             UIManager.setLookAndFeel(UIManager.createLookAndFeel("Windows"));
         } catch (UnsupportedLookAndFeelException e) {
@@ -51,9 +53,34 @@ public class erfassen extends JFrame{
         nameLabel.setText("Angemeldet als " + currentUser.getVorname() + " " + currentUser.getName() + " (" + currentUser.getMitarbeiternummer() + ").");
 
 
-
         setVisible(true);
+
+        //Button Funktion Zuweisung
+
+        suchenToggleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Suchen nach Schwarzfahrer anhand von Ausweisnummer
+            }
+        });
+
+        speichernToggleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                dispose();
+            }
+        });
+
+        abbrechenToggleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                dispose();
+            }
+        });
     }
+
 
     public static void open() {
         erfassen gui = new erfassen();
